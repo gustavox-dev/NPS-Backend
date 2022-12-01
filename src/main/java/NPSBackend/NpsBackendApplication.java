3e8c355402e54e52b7a30fba17bf767ad57229e8
Gustavo <guh.coostta@gmail.com>
2022-12-01 17:41:46 -0300

    @gustavox-dev :construction_worker: Configuração inicial do projeto

---
+++ src/main/java/NPSBackend/NpsBackendApplication.java
@@ -0,0 +1,13 @@
+package NPSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsBackendApplication.class, args);
+	}
+
+}
